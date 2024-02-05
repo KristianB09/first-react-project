f44afcbd6ab1daf45ccaeefa12f491c8b5fdc4bd
kristianb09 <kristianb09.kodehode@gmail.com>
2024-02-05 12:45:03 +0100

    format change because prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-
 import "./App.css";
 import dog from "./assets/doggo.gif";
 
 function App() {
-  
   return (
-  <div>
-    <h1>Hello world</h1>
-    <img src={dog} alt="A dog with sunglasses going fast on a jetski!" />
-  </div>)
+    <div>
+      <h1>Hello world</h1>
+      <img src={dog} alt="A dog with sunglasses going fast on a jetski!" />
+    </div>
+  );
 }
 
 export default App;
