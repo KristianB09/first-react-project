4a766ee9c3bd1048357f0452370e3574c1f3f8d9
KristianB09 <kristianb09.kodehode@gmail.com>
2024-02-07 13:29:14 +0100

    Added card images from mtg.

---
+++ src/getData.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "./getData.css";
+
+let mtgUrl = "https://api.magicthegathering.io/v1/cards";
+
+let mtgList = [];
+
+getData();
+
+async function getData(url = mtgUrl) {
+  const response = await fetch(url);
+  if (response.ok !== true) {
+    console.log("Something went wrong");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function updateMtgList(url) {
+  mtgList = await getData(url);
+}
+
+function Card() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchCards() {
+      await updateMtgList(mtgUrl);
+      if (mtgList.cards) {
+        setCards(mtgList.cards);
+      }
+    }
+
+    fetchCards();
+  }, []);
+
+  if (cards.length === 0) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="cards-container">
+      {cards.map(
+        (card, index) =>
+          card.imageUrl && (
+            <div key={index} className="card-container">
+              <img key={index} src={card.imageUrl} alt={card.name} />
+            </div>
+          )
+      )}
+    </div>
+  );
+}
+
+export default Card;
